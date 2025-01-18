10b63c792d069fd183064f36b0e81f98efa3fb42
Hary <harysamardzic01@gmail.com>
2025-01-18 16:17:53 +0100

    root router setup

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,13 +1,22 @@
-import express, { Request, Response } from "express";
+import express, { Request, Response } from 'express';
+import bodyParser from 'body-parser';
+import userRouter from './user/router/UserRouter';
+import postRouter from './post/router/PostRouter';
+import commentRouter from './comment/router/CommentRouter';
 
 const app = express();
-const port = 3000;
+const port = 4000;
 
-app.get("/", (req: Request, res: Response) => {
-  res.send("Hello World!");
+app.use(bodyParser.json());
+
+app.get('/', (req: Request, res: Response) => {
+  res.status(404).send('Endpoint not in use');
 });
+
+app.use('/user', userRouter);
+app.use('/post', postRouter);
+app.use('/comment', commentRouter);
 
 app.listen(port, () => {
   console.log(`Server is running at http://localhost:${port}`);
 });
-
