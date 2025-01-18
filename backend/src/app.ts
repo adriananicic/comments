9f9dfda6b790af3297842e3f66b0dd16df1ed7e5
Hary <harysamardzic01@gmail.com>
2025-01-18 16:35:39 +0100

    cors and dotenv setup

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -3,16 +3,22 @@ import bodyParser from 'body-parser';
 import userRouter from './user/router/UserRouter';
 import postRouter from './post/router/PostRouter';
 import commentRouter from './comment/router/CommentRouter';
+import cors from 'cors';
+import dotenv from 'dotenv';
 
 const app = express();
 const port = 4000;
-
+dotenv.config();
 app.use(bodyParser.json());
 
 app.get('/', (req: Request, res: Response) => {
   res.status(404).send('Endpoint not in use');
 });
-
+app.use(
+  cors({
+    origin: process.env.CLIENT_URL,
+  })
+);
 app.use('/user', userRouter);
 app.use('/post', postRouter);
 app.use('/comment', commentRouter);
