c07a72a07ddd0dbdda928511edca89f4d7bb337c
Hary <harysamardzic01@gmail.com>
2025-01-18 16:39:55 +0100

    UserRouter fix

--- backend/src/user/router/UserRouter.ts
+++ backend/src/user/router/UserRouter.ts
@@ -1,11 +1,11 @@
-import { NextFunction, Response, Router } from 'express';
+import { Response, Router, Request } from 'express';
 import { PrismaUserRepository } from '../repository/PrismaUserRepository';
 import { UserController } from '../controller/UserController';
 
 const userRouter = Router();
 const userRepository = new PrismaUserRepository();
 const userController = new UserController(userRepository);
-userRouter.get('/list', async (res: Response) => {
+userRouter.get('/list', async (_req: Request, res: Response) => {
   try {
     const users = await userController.getAllUsers();
     res.status(200).json({ data: users });
