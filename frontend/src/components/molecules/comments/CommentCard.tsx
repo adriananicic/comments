a0a80699955f90c25102b39782b85edddf6e9467
Adrian <adriananicic@gmail.com>
2025-01-30 17:10:34 +0100

    fix reply icon not shown

--- frontend/src/components/molecules/comments/CommentCard.tsx
+++ frontend/src/components/molecules/comments/CommentCard.tsx
@@ -37,48 +37,52 @@ const CommentCard: FC<ICommentCardProps> = ({
   };
 
   return (
-    <div className="flex items-start justify-start gap-3 w-full ">
-      <img
-        height={48}
-        width={48}
-        className="rounded-full object-cover w-[48px] h-[48px] flex-shrink-0"
-        src={comment.commenter.profilePicture}
-        alt={comment.commenter.name}
-      />
-      <div className="flex flex-col gap-3 overflow-hidden ">
-        <div className="flex  items-center gap-2">
-          <div
-            onClick={() => setShowReplies((prev) => !prev)}
-            className="flex  flex-col bg-primary-weak rounded-md border-[1px] border-primary-medium p-6 gap-3 cursor-pointer break-words overflow-hidden text-ellipsis "
-          >
-            <p className="text-primary-strong body-1">
-              {comment.commenter.name}
-            </p>
-            <CommentRenderer text={comment.body} />
+    <div className="flex items-start justify-start gap-3 w-full flex-col ">
+      <div className="flex items-start justify-start gap-3 w-full">
+        <img
+          height={48}
+          width={48}
+          className="rounded-full object-cover w-[48px] h-[48px] flex-shrink-0"
+          src={comment.commenter.profilePicture}
+          alt={comment.commenter.name}
+        />
+        <div className="flex flex-col gap-3 overflow-hidden ">
+          <div className="flex  items-center gap-2">
+            <div
+              onClick={() => setShowReplies((prev) => !prev)}
+              className="flex  flex-col bg-primary-weak rounded-md border-[1px] border-primary-medium p-6 gap-3 cursor-pointer break-words overflow-hidden text-ellipsis "
+            >
+              <p className="text-primary-strong body-1">
+                {comment.commenter.name}
+              </p>
+              <CommentRenderer text={comment.body} />
+            </div>
+            {comment.commenter.userId === userId && (
+              <Button
+                loading={isCommentDeleting}
+                buttonType="danger"
+                icon="delete"
+                onClick={() => {
+                  handleDeleteClick(comment.commentId);
+                }}
+              />
+            )}
           </div>
-          {comment.commenter.userId === userId && (
-            <Button
-              loading={isCommentDeleting}
-              buttonType="danger"
-              icon="delete"
-              onClick={() => {
-                handleDeleteClick(comment.commentId);
-              }}
-            />
-          )}
+          <div className="flex gap-2 items-center">
+            <p className="text-base text-primary font-medium">
+              {formatTimeFromTimestamp(comment.timestamp)}
+            </p>
+            <Icon size={2} className="bg-primary" icon="dot" />
+            <p
+              onClick={() => handleReplyClick(comment.body)}
+              className="text-base font-medium text-accent cursor-pointer"
+            >
+              Reply {comment.noOfReplies ? `(${comment.noOfReplies})` : ''}
+            </p>
+          </div>
         </div>
-        <div className="flex gap-2 items-center">
-          <p className="text-base text-primary font-medium">
-            {formatTimeFromTimestamp(comment.timestamp)}
-          </p>
-          <Icon size={2} className="bg-primary" icon="dot" />
-          <p
-            onClick={() => handleReplyClick(comment.body)}
-            className="text-base font-medium text-accent cursor-pointer"
-          >
-            Reply {comment.noOfReplies ? `(${comment.noOfReplies})` : ''}
-          </p>
-        </div>
+      </div>
+      <div className="left-6 relative">
         {showReplies &&
           comment.childComments?.map(
             (reply: getCommentReturn, index: number) => (
--- frontend/src/components/molecules/comments/CommentReply.tsx
+++ frontend/src/components/molecules/comments/CommentReply.tsx
@@ -15,21 +15,22 @@ const CommentReply: FC<ICommentReplyProps> = ({
   index,
   reply,
   refetchComments,
-    fetchReplies,
+  fetchReplies,
 }) => {
   const postId = usePathname().split('/')[2];
-  
+
   return (
     <div
       onClick={(e) => {
-        e.stopPropagation()
-        reply.noOfReplies > 0 ? fetchReplies(reply) : null}}
-      className="flex items-start gap-3 relative"
+        e.stopPropagation();
+        reply.noOfReplies > 0 ? fetchReplies(reply) : null;
+      }}
+      className="flex items-start gap-3 z-50   "
     >
       {index === 0 && (
         <Icon
           icon="reply"
-          className="bg-primary absolute opacity-45 -left-9 "
+          className="bg-primary z-[200]  opacity-45  -left-9 w-full "
         />
       )}
       <CommentCard
@@ -37,9 +38,7 @@ const CommentReply: FC<ICommentReplyProps> = ({
         refetchComments={refetchComments}
         fetchReplies={fetchReplies}
       />
-      
     </div>
-       
   );
 };
 
